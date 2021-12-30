bd3a8d5f8cb836ba06bf78203b5840696b2c54b4
Tori McQuinn <tmcquinn@stripe.com>
2021-12-30 17:03:43 -0500

    whoops accidentally used ()

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className='App'>
-      <SearchBox placeholder='search monsta' handleChange={this.handleChange()}/>
+      <SearchBox placeholder='search monsta' handleChange={this.handleChange}/>
         <CardList monsters={filteredMonsters} />
       </div>
     );
